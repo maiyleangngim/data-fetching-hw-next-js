47e060ae4dc9c967c05a902a7a3c7a4a1e908daf
Maiy Leangnim <2024020maiy@aupp.edu.kh>
2026-06-03 11:47:49 +0700

    forgot to change root for sitemap

--- src/app/sitemap.tsx
+++ src/app/sitemap.tsx
@@ -3,19 +3,19 @@ import type { MetadataRoute } from 'next'
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'http://localhost:3000',
+      url: 'https://data-fetching-hw-next-js-197g.vercel.app',
       lastModified: new Date(),
       changeFrequency: 'yearly',
       priority: 1,
     },
     {
-      url: 'http://localhost:3000/dashboard',
+      url: 'https://data-fetching-hw-next-js-197g.vercel.app/dashboard',
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 0.8,
     },
     {
-      url: 'http://localhost:3000/dashboard/products',
+      url: 'https://data-fetching-hw-next-js-197g.vercel.app/dashboard/products',
       lastModified: new Date(),
       changeFrequency: 'weekly',
       priority: 0.5,
